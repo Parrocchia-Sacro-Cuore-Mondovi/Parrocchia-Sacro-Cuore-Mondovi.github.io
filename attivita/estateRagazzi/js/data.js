b4f980c82c0122cc32ba778fd4e4de042d7b26a2
MikiToast <152236175+MikiToast@users.noreply.github.com>
2026-07-17 12:49:41 +0200

    Added Estate Ragazzi

---
+++ attivita/estateRagazzi/js/data.js
@@ -0,0 +1,52 @@
+const locandineDb = [
+    {
+        "nome": "2026_eventi.png",
+        "data": "Eventi Estate Ragazzi 2026"
+    },
+    {
+        "nome": "2026_volantino_instagram.png",
+        "data": "Moduli Iscrizioni Estate Ragazzi 2026"
+    }
+];
+
+const eventiDb = [
+    {
+        "data": "3/09",
+        "ore": "7:45/19:00",
+        "titolo": "Gita a \"Cupole Lido\"",
+        "desc": "Iscrizioni entro lunedì 31 agosto."
+    },
+    {
+        "data": "9/09",
+        "ore": "7:45/19:00",
+        "titolo": "Gita al \"Gran Paradiso\"",
+        "desc": "Iscrizioni entro mercoledì 2 settembre."
+    },
+    {
+        "data": "11/09",
+        "ore": "14:30/21:00",
+        "titolo": "Festa di fine Estate Ragazzi",
+        "desc": "Iscrizioni entro lunedì 7 settembre."
+    }
+];
+
+const iscrizioniDb = [
+    {
+        "titolo": "Iscrizioni Estate Ragazzi 2026",
+        "desc": "31 agosto - 11 settembre 2026",
+        "link": "https://forms.gle/u8ayMuttz2nVUiUH6"
+    }
+];
+
+const documentiDb = [
+    {
+        "titolo": "Autorizzazioni Estate Ragazzi 2026",
+        "desc": "Estate Ragazzi 2026",
+        "link": "/attivita/estateRagazzi/pdf/2026_autorizzazioni.pdf"
+    },
+    {
+        "titolo": "Patto di responsabilità parrocchia e famiglie (2026-2027)",
+        "desc": "2026-2027",
+        "link": "/attivita/estateRagazzi/pdf/patto_resp_2026-2027.pdf"
+    }
+];
