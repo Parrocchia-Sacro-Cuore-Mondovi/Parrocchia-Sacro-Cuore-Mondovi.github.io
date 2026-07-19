e5b85d6b1f17ae96751f8547a5169037e93b99a9
MikiToast <152236175+MikiToast@users.noreply.github.com>
2026-07-19 19:44:43 +0200

    Added amico pasqua 2026

--- amico/js/data.js
+++ amico/js/data.js
@@ -1,14 +1,8 @@
-const fogliettiDb = [
-    "2026_01_24.png"
-];
+const fogliettiDb = [];
 
 const bollettiniDb = [
     {
         "titolo": "Aprile 2026",
         "file": "2026_04.pdf"
-    },
-    {
-        "titolo": "Maggio 2026",
-        "file": "2026_05.pdf"
     }
 ];
