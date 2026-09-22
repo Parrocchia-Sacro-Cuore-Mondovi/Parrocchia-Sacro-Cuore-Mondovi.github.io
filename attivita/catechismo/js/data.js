c76f9368f180b48e46778a4c4cd96fca499fe839
MikiToast <152236175+MikiToast@users.noreply.github.com>
2026-09-22 18:23:02 +0200

    update 22/09/26

--- attivita/catechismo/js/data.js
+++ attivita/catechismo/js/data.js
@@ -1,4 +1,9 @@
-const locandineDb = [];
+const locandineDb = [
+    {
+        "nome": "2026_10_03.webp",
+        "data": "3 Ottobre 2026"
+    }
+];
 
 const documentiDb = [
     {
@@ -11,7 +16,57 @@ const documentiDb = [
 const eventiDb = [
     {
         "data": "17/10",
-        "ore": "17:00-18:00",
-        "titolo": "Inizia il Catechismo!"
+        "ore": "17:00-18:15",
+        "titolo": "Inizia il catechismo!"
+    },
+    {
+        "data": "14/10",
+        "ore": "20:45-22:00",
+        "titolo": "Incontro per iscrivere i bambini al catechismo in oratorio"
+    },
+    {
+        "data": "12/12",
+        "ore": "17:50-18:20",
+        "titolo": "Variazione orario catechismo"
+    },
+    {
+        "data": "19/12",
+        "ore": "17:50-18:20",
+        "titolo": "Variazione orario catechismo"
+    },
+    {
+        "data": "16/12",
+        "ore": "20:30-21",
+        "titolo": "Novena di Natale in chiesa animata dai ragazzi delle medie"
+    },
+    {
+        "data": "17/12",
+        "ore": "20:30-21",
+        "titolo": "Novena di Natale in chiesa animata dai bambini delle elementari"
+    },
+    {
+        "data": "18/12",
+        "ore": "20:30-21",
+        "titolo": "Novena di Natale in chiesa animata dai ragazzi delle medie"
+    },
+    {
+        "data": "21/12",
+        "ore": "20:30-21",
+        "titolo": "Novena di Natale in chiesa animata dai bambini delle elementari"
+    },
+    {
+        "data": "22/12",
+        "ore": "20:30-21",
+        "titolo": "Novena di Natale in chiesa animata dai ragazzi delle medie"
+    },
+    {
+        "data": "23/12",
+        "ore": "20:30-21",
+        "titolo": "Novena di Natale in chiesa animata dai bambini delle elementari"
+    },
+    {
+        "data": "16/01",
+        "ore": "17:00-18:15",
+        "titolo": "Ricomincia il catechismo!"
     }
 ];
--- attivita/oratorio/js/data.js
+++ attivita/oratorio/js/data.js
@@ -1,4 +1,9 @@
-const locandineDb = [];
+const locandineDb = [
+    {
+        "nome": "2026_10_03.webp",
+        "data": "3 Ottobre 2026"
+    }
+];
 
 const documentiDb = [
     {
@@ -11,7 +16,22 @@ const documentiDb = [
 const eventiDb = [
     {
         "data": "3/10",
-        "ore": "14:30/18:00",
+        "ore": "14:30-18:00",
         "titolo": "Ricomincia l'Oratorio!"
+    },
+    {
+        "data": "17/10",
+        "ore": "14:30-17:00",
+        "titolo": "Inizio prove recita di Natale"
+    },
+    {
+        "data": "23/12",
+        "ore": "21:30-23:00",
+        "titolo": "Recita di Natale"
+    },
+    {
+        "data": "6/03",
+        "ore": "14:00-18:00",
+        "titolo": "Festa della pace"
     }
 ];
